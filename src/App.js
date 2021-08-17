a70f56e2697c8c71af8ca5c03dcde5a1781901a9
Soul-Remix <soulremix-webdev@pm.me>
2021-08-17 16:28:14 +0300

    add score and cards shuffling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import pokeArr from './Components/images';
 import Card from './Components/Card';
 import Header from './Components/Header';
 import Score from './Components/Score';
+import shuffle from './Components/shuffle';
 import './styles/app.css';
 
 function App() {
-  const arr = pokeArr.map((x) => {
-    return <Card key={x.id} name={x.name} src={x.src} />;
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [pokemon, setPokemon] = useState(pokeArr);
+  const [win, setWin] = useState(false);
+  const [lose, setLose] = useState(false);
+
+  useEffect(() => {
+    setPokemon(shuffle(pokemon));
+  }, [score]);
+
+  const handleClick = (id) => {
+    const clicked = pokemon.filter((x) => {
+      return x.id === id && x.isClicked === undefined;
+    });
+    console.log(clicked);
+    if (clicked.length === 0) {
+      if (score > bestScore) {
+        setBestScore(score);
+      }
+      return;
+    } else {
+      setScore(score + 1);
+    }
+    setPokemon(
+      pokemon.map((x) => {
+        if (x.id === id) {
+          return { ...x, isClicked: true };
+        }
+        return x;
+      })
+    );
+  };
+
+  const arr = shuffle(pokemon).map((x) => {
+    return (
+      <Card
+        key={x.id}
+        name={x.name}
+        src={x.src}
+        id={x.id}
+        click={() => handleClick(x.id)}
+      />
+    );
   });
   return (
     <div>
       <Header />
-      <Score />
+      <Score currentScore={score} bestScore={bestScore} />
       <div className="container">{arr}</div>
     </div>
   );
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Card(props) {
   return (
-    <div className="card">
+    <div className="card" onClick={props.click} id={props.id}>
       <div>
         <img src={props.src} alt={props.name} />
       </div>
