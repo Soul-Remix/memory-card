b1178311f3a6063d11b3d06ec1924053edd2ad06
Soul-Remix <soulremix-webdev@pm.me>
2021-08-17 15:29:01 +0300

    add component to app and add styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import React from 'react';
+import pokeArr from './Components/images';
+import Card from './Components/Card';
+import Header from './Components/Header';
+import Score from './Components/Score';
+import './styles/app.css';
+
 function App() {
-  return <div></div>;
+  const arr = pokeArr.map((x) => {
+    return <Card key={x.id} name={x.name} src={x.src} />;
+  });
+  return (
+    <div>
+      <Header />
+      <Score />
+      <div className="container">{arr}</div>
+    </div>
+  );
 }
 
 export default App;
