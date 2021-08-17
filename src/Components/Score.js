eda5d1944163b23f12974d067226e3a9cc6ef91c
Soul-Remix <soulremix-webdev@pm.me>
2021-08-17 15:42:13 +0300

    create score tags

--- src/Components/Score.js
+++ src/Components/Score.js
@@ -3,8 +3,10 @@ import React from 'react';
 function Score(props) {
   return (
     <div className="score">
-      <div className="best-score">Best score: {props.bestScore}</div>
-      <div className="current-score">Current score: {props.currentScore}</div>
+      <div className="best-score score-tag">Best score: {props.bestScore}</div>
+      <div className="current-score score-tag">
+        Current score: {props.currentScore}
+      </div>
     </div>
   );
 }
